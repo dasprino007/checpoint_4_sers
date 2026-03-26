c7400b7b205ff2e3a25e4f7adc53505602bf5134
Diego Leite Asprino <diegoasprino07@gmail.com>
2026-03-26 19:12:03 -0300

    primeira estrutura de dados feita

---
+++ lista.py
@@ -0,0 +1,70 @@
+import random
+import memory_profiler
+import consts
+from datetime import datetime
+from pyJoules.energy_meter import EnergyContext
+from pyJoules.device.rapl_device import RaplPackageDomain
+from pyJoules.device.nvidia_device import NvidiaGPUDomain
+from pyJoules.handler.csv_handler import CSVHandler
+
+def insercoes_lista(lista : list, qnt : int):
+    for i in range(qnt):
+        lista.append(i)
+
+def remocoes_lista(lista : list, qnt : int):
+    for i in range(qnt):
+        lista.remove(i) # tenta remover o numero
+
+def busca_lista(lista : list, qnt : int):
+    for i in range(qnt):
+        num = random.randint(1,len(lista) - 1) # pega um numero aleatorio 
+        lista.index(num) # procura o numero e retorna o indice dele
+
+def testar_memoria_lista():
+    lista = []
+    memory_usage_insercao = memory_profiler.memory_usage((insercoes_lista, (), {'lista':lista,'qnt':consts.QNT_INS}), max_usage=True) # retorna o uso de memoria maxima
+    memory_usage_busca = memory_profiler.memory_usage((busca_lista, (), {'lista':lista,   'qnt':consts.QNT_BUSCA}), max_usage=True) # retorna o uso de memoria maxima
+    memory_usage_remocao = memory_profiler.memory_usage((remocoes_lista, (), {'lista':lista, 'qnt':consts.QNT_REM}), max_usage=True) # retorna o uso de memoria maxima
+
+    print(memory_usage_insercao)
+    print(memory_usage_busca)
+    print(memory_usage_remocao)
+    pass
+
+def testar_tempo_lista():
+    lista = []
+    start_time : datetime
+    tempo_exec_insert : datetime
+    tempo_exec_busca : datetime
+    tempo_exec_remocao : datetime
+
+    start_time = datetime.now()
+    insercoes_lista(lista, consts.QNT_INS)
+    tempo_exec_insert = datetime.now() - start_time
+
+    start_time = datetime.now()
+    busca_lista(lista, consts.QNT_BUSCA)
+    tempo_exec_busca = datetime.now() - start_time
+
+    start_time = datetime.now()
+    remocoes_lista(lista, consts.QNT_REM)
+    tempo_exec_remocao = datetime.now() - start_time
+
+    print(tempo_exec_insert)
+    print(tempo_exec_busca)
+    print(tempo_exec_remocao)
+    pass
+
+csv_handler = CSVHandler('data/lista-insercao.csv')
+
+def testar_energia_lista():
+    lista = []
+    with EnergyContext(handler=csv_handler, start_tag='lista') as ctx:
+        ctx.record(tag='lista-insercao')
+        insercoes_lista(lista, consts.QNT_INS)
+        ctx.record(tag='lista-busca')
+        busca_lista(lista, consts.QNT_BUSCA)
+        ctx.record(tag='lista-remocao')
+        remocoes_lista(lista, consts.QNT_REM)
+
+    csv_handler.save_data()
---
+++ main.py
@@ -0,0 +1,10 @@
+import lista
+
+def main():
+    #lista.testar_memoria_lista()
+    #lista.testar_tempo_lista()
+    lista.testar_energia_lista()
+
+
+if __name__ == "__main__":
+    main()
